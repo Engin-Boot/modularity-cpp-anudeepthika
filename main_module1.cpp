4d7b95e51a9ff64ff5bf18f8f6b2c9cf9b1b2268
anudeepthika <55407536+anudeepthika@users.noreply.github.com>
2020-08-24 18:24:31 +0530

    Update mainmodule

--- main_module1.cpp
+++ main_module1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <assert.h>
-#include "namespace.h"
+#include "namespace_file.h"
 
 void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
